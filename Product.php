ce7a0baebfccecf7c59395b718c3d211c78e4a2b
Ali Siddique <alisiddique2011@gmail.com>
2023-10-03 14:44:59 +0600

    IC

--- Product.php
+++ Product.php
@@ -2,9 +2,15 @@
 
 class Product {
     // TODO: Add properties
+    public $id;
+    public $name;
+    public $price;
 
     public function __construct( $id, $name, $price ) {
         // TODO: Initialize properties
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
     }
 
     // TODO: Add getFormattedPrice method
